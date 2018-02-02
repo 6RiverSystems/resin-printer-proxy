67672e3143ff6164cc4cf0f474c281ba5108a902
Joseph Hughes <jjhughes57@gmail.com>
2018-02-02 16:30:46 -0500

    fix: debug

--- app/activate_connection.py
+++ app/activate_connection.py
@@ -9,37 +9,30 @@ import sys
 # name = sys.argv[1]
 connections = NetworkManager.Settings.ListConnections()
 connections = dict([(x.GetSettings()['connection']['id'], x) for x in connections])
-#conn = connections[name]
-print(connections)
+conn = connections['resin-hotspot']
+print(conn)
 # Find a suitable device
-# ctype = conn.GetSettings()['connection']['type']
-# if ctype == 'vpn':
-#     for dev in NetworkManager.NetworkManager.GetDevices():
-#         if dev.State == NetworkManager.NM_DEVICE_STATE_ACTIVATED and dev.Managed:
-#             break
-#     else:
-#         print("No active, managed device found")
-#         sys.exit(1)
+ctype = conn.GetSettings()['connection']['type']
+
 # else:
-# dtype = {
-#     '802-11-wireless': NetworkManager.NM_DEVICE_TYPE_WIFI,
-#     '802-3-ethernet': NetworkManager.NM_DEVICE_TYPE_ETHERNET,
-#     'gsm': NetworkManager.NM_DEVICE_TYPE_MODEM,
-# }.get(ctype,ctype)
+dtype = {
+    '802-11-wireless': NetworkManager.NM_DEVICE_TYPE_WIFI,
+    '802-3-ethernet': NetworkManager.NM_DEVICE_TYPE_ETHERNET,
+    'gsm': NetworkManager.NM_DEVICE_TYPE_MODEM,
+}.get(ctype,ctype)
+print(dtype)
+
 devices = NetworkManager.NetworkManager.GetDevices()
 
 for dev in devices:
-    if dev.DeviceType == NetworkManager.NM_DEVICE_TYPE_WIFI:
+    if dev.DeviceType == dtype and dev.State == NetworkManager.NM_DEVICE_STATE_DISCONNECTED:
         print("WIFI DeviceType: {} State: {}".format(dev.DeviceType,dev.State))
-        print(NetworkManager.NM_DEVICE_STATE_DISCONNECTED)
         print(dev)
-        
-    # if dev.DeviceType == dtype and dev.State == NetworkManager.NM_DEVICE_STATE_DISCONNECTED:
-    #     print(dev)
-    #     break
+        break
     # else:
     #     print("No suitable and available %s device found" % ctype)
     #     sys.exit(1)
 # 
 # # And connect
+print(dev)
 # NetworkManager.NetworkManager.ActivateConnection(conn, dev, "/")
