7d03d6f42bb57b80357521d9ae4a76b8f70bb82f
Joseph Hughes <jjhughes57@gmail.com>
2018-02-05 10:50:18 -0500

    fix: hotspot up!

--- app/app.sh
+++ app/app.sh
@@ -2,5 +2,14 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+sleep 5
 
-python ./activate_connection.py
+ifconfig wlan0 down
+
+sleep 2
+
+ifconfig wlan0 up
+
+sleep 5
+
+python ./hotspot.py wlan0 up
