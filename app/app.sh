5211185d9dd451db5a8dd37a5b62a2ba916744c4
Joseph Hughes <jjhughes57@gmail.com>
2018-02-06 10:02:58 -0500

    fix: iptables

--- app/app.sh
+++ app/app.sh
@@ -72,5 +72,8 @@ else
   echo "Zerotier Network Added: ${ZEROTIER_NETWORK}"
 fi
 
+# iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE  
+# iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT  
+# iptables -A FORWARD -i wlan1 -o wlan0 -j ACCEPT
 
 nginx -g 'daemon off;'
