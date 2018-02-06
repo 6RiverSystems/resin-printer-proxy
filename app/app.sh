917c9af374b64dd2578525fb99a1d0eefbc05bf6
Joseph Hughes <jjhughes57@gmail.com>
2018-02-05 20:52:22 -0500

    fix: use stretch

--- app/app.sh
+++ app/app.sh
@@ -1,7 +1,32 @@
 #!/bin/ash
+# shellcheck shell=dash 
+set -ex
 
 : ${PRINTER_IP?"Need to set PRINTER_IP"}
+: ${ZEROTIER_NETWORK?"Need to set ZEROTIER_NETWORK"}
+
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# Choose a condition for running WiFi Connect according to your use case:
+
+# 1. Is there a default gateway?
+# ip route | grep default
+
+# 2. Is there Internet connectivity?
+# nmcli -t g | grep full
+
+# 3. Is there Internet connectivity via a google ping?
+wget --spider http://google.com 2>&1
+
+# 4. Is there an active WiFi connection?
+#iwgetid -r
+
+if [ $? -eq 0 ]; then
+    printf 'Skipping WiFi Connect\n'
+else
+    printf 'Starting WiFi Connect\n'
+    ./wifi-connect -s pp-wifi-setup -p 6rsprinter -i wlan0
+fi
 
 envsubst < /etc/nginx/nginx-template.conf > /etc/nginx/nginx.conf 
 envsubst < /etc/nginx/printer-template > /etc/nginx/sites-avaliable/printer
