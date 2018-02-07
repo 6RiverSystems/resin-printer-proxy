0aa72863b377148853031ac56177f08d1cf6cf0a
Joseph Hughes <jjhughes57@gmail.com>
2018-02-06 22:54:28 -0500

    update wifi network

--- app/save_connection.py
+++ app/save_connection.py
@@ -5,7 +5,8 @@ import NetworkManager
 
 for conn in NetworkManager.NetworkManager.ActiveConnections:
     settings = conn.Connection.GetSettings()
-    print("Looking at connection {}".format(settings['connection']['id']))
-    if 'wlan1' in conn.Devices or 'wlan0' in conn.devices:
-        print("Saving connection info for {}".format(settings['connection']['id']))
-        conn.Connection.Save()
+    if conn.Devices:
+        devices = [x.Interface for x in conn.Devices]
+        if 'wlan1' in devices or 'wlan0' in devices:
+            print("Saving connection info for {}".format(settings['connection']['id']))
+            conn.Connection.Save()
