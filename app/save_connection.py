69e5054760a63578ad19a92f18ceea4e2fd6c31c
Joseph Hughes <jjhughes57@gmail.com>
2018-02-06 23:55:57 -0500

    update wifi network

--- app/save_connection.py
+++ app/save_connection.py
@@ -4,11 +4,19 @@ Save Settings that don't get saved
 import NetworkManager, pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+for resincon in NetworkManager.NetworkManager.Connections:
+    resin_settings = resincon.Connection.GetSettings()
+    if resin_settings['connection']['id'] = 'resin-wifi-01':
+        break
+
 for conn in NetworkManager.NetworkManager.ActiveConnections:
     settings = conn.Connection.GetSettings()
     if settings['connection']['uuid'] != '2b0d0f1d-b79d-43af-bde1-71744625642e' and conn.Devices:
         devices = [x.Interface for x in conn.Devices]
         if 'wlan1' in devices or 'wlan0' in devices:
+            if resincon.Connection.GetSettings()['connection']['uuid'] != settings['connection']['uuid']:
+                print('Deleting Old Connection')
+                resincon.Connection.Delete()
             print("Saving connection for {}".format(settings['connection']['id']))
             settings['connection']['id'] = 'resin-wifi-01'
             conn.Connection.Update(settings)
