0270c1c657824557aa395c0b8a049ef6f99691f2
Joseph Hughes <jjhughes57@gmail.com>
2018-02-06 23:08:59 -0500

    update wifi network

--- app/save_connection.py
+++ app/save_connection.py
@@ -1,12 +1,13 @@
 """
 Save Settings that don't get saved
 """
-import NetworkManager
+import NetworkManager, uuid
 
 for conn in NetworkManager.NetworkManager.ActiveConnections:
     settings = conn.Connection.GetSettings()
-    if conn.Devices:
+    if settings['connection']['uuid'] != '2b0d0f1d-b79d-43af-bde1-71744625642e' and conn.Devices:
         devices = [x.Interface for x in conn.Devices]
         if 'wlan1' in devices or 'wlan0' in devices:
-            print("Saving connection info for {}".format(settings['connection']['id']))
-            NetworkManager.Settings.AddConnection(conn)
+            print("Saving connection for {}".format(settings['connection']['id']))
+            settings['connection']['uuid'] = str(uuid.uuid4())
+            NetworkManager.Settings.AddConnection(settings)
