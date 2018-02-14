54777f321cfc9b9dc3d9f7bad86e1619ba6534b2
Joseph Hughes <jjhughes57@gmail.com>
2018-02-14 11:13:43 -0500

    fix: add traceroute

--- app/app.sh
+++ app/app.sh
@@ -93,6 +93,6 @@ until ping -c1 ${PRINTER_IP} &>/dev/null; do :; done
 echo "Found printer starting proxy...."
 
 echo "Running forever ping..."
-ping -i 1 ${PRINTER_IP} &>/dev/null &
+ping ${PRINTER_IP} &>/dev/null &
 
 nginx -g 'daemon off;'
