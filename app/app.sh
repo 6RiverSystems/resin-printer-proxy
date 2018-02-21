12c5be8b6ed12b3c765a3a1ba057d63a7bb0b793
Joseph Hughes <jjhughes57@gmail.com>
2018-02-21 14:37:37 -0500

    fix: wlan1

--- app/app.sh
+++ app/app.sh
@@ -42,7 +42,7 @@ if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect -s pp-wifi-setup -p 6rsprinter
+    ./wifi-connect -s pp-wifi-setup -p 6rsprinter -i wlan1
     sleep 5
 fi
 
